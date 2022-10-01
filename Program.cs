a1238f034fc12de83b9c3b2bd581880f014390a8
Денис Шурыгин <disa-23ru@rambler.ru>
2022-10-01 20:55:04 +0300

    первая задача

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+
+Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+int Exp(int numA, int numB){
+  int result = 1;
+  for(int i=1; i <= numB; i++){
+    result = result * numA;
+  }
+    return result;
+}
+int exponentiation = Exp(numA, numB);
+Console.WriteLine("Число {0} в степени {1} равана {2}", numA, numB, exponentiation);
+
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12*/
+
+Console.Write("Введите число: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
