7194fe667d71739f78d335b8ccbce53814277ee0
Денис Шурыгин <disa-23ru@rambler.ru>
2022-10-02 21:30:27 +0300

    вторая и третья задачи

--- Program.cs
+++ Program.cs
@@ -24,7 +24,54 @@ Console.WriteLine("Число {0} в степени {1} равана {2}", numA,
 
 82 -> 10
 
-9012 -> 12*/
+9012 -> 12
 
-Console.Write("Введите число: ");
-int numbers = Convert.ToInt32(Console.ReadLine());
+int number = ReadInt("Введи число: ");
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int NumberLen(int a){
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+void SumNumbers(int n, int len){
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine("Сумма введеных чисел = " + sum);
+}
+
+
+Задача 29: Напишите программу, которая задаёт массив из 8 рандомных элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]*/
+
+int lenArray = ReadInt("Введи длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(0,99);
+    Console.Write(randomArray[i] + " ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
